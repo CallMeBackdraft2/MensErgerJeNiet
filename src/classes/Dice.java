cba3dd5ba8b65dc705d8f13f09edebca9b85f73d
CallMeBackdraft <dennis.aspers@gmail.com>
2018-09-10 17:51:17 +0200

    DiceNumber enum done Dice class should be done as well

--- src/classes/Dice.java
+++ src/classes/Dice.java
@@ -1,5 +1,29 @@
 package classes;
 
-public class Dice {
+import enums.diceNumber;
 
+import java.util.Date;
+import java.util.Random;
+
+public class Dice {
+    private diceNumber rolledValue;
+    private long randomSeed;
+
+    public Dice(){
+        //use the current date and time converted to a long to create the seed for the random number generator
+        this.randomSeed = new Date().getTime();
+    }
+
+    public diceNumber rollDice(){
+        //instantiate the random number generator with the randomSeed value as seed
+        Random rand = new Random(randomSeed);
+        //use the random number generator to get a random value between 0 and 5 then adding one to get the correct bounds for the dice
+        //this gets cast into the diceNumber enum to store the correct vallue
+        this.rolledValue = diceNumber.values()[rand.nextInt(6) + 1];
+        return rolledValue;
+    }
+
+    public diceNumber getRolledValue(){
+        return this.rolledValue;
+    }
 }
--- src/enums/diceNumber.java
+++ src/enums/diceNumber.java
@@ -1,4 +1,16 @@
 package enums;
 
 public enum  diceNumber {
+    ONE     (1),
+    TWO     (2),
+    THREE   (3),
+    FOUR    (4),
+    FIVE    (5),
+    SIX     (6);
+
+    private int value;
+
+    diceNumber(int i) {
+        this.value = i;
+    }
 }
