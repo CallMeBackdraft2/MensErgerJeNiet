50c9f40c68cc929677088e5af201453d893d99bd
CallMeBackdraft <dennis.aspers@gmail.com>
2018-09-10 21:11:46 +0200

    Almost done with 4 player board added pawnstate enum added tile class added arrow 35 img

--- src/classes/Pawn.java
+++ src/classes/Pawn.java
@@ -1,4 +1,14 @@
 package classes;
 
+import enums.pawnState;
+
 public class Pawn {
+    private int pawnID;
+    private int pawnLoc;
+    private pawnState pawnstate;
+
+    public Pawn(){
+        //every newly instantiated pawn always starts in one of the four start positions of the player
+        this.pawnstate = pawnState.STARTPOSITION;
+    }
 }
--- src/ui/Main.java
+++ src/ui/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("4-Player.fxml"));
         primaryStage.setTitle("Mens Erger Je Niet");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
