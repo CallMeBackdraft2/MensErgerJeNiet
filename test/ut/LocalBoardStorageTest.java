caeb544f5a03937a7f22403bbcd52fa54fed68fd
Hugo <git@hugoblok.nl>
2018-10-29 09:05:30 +0100

    Added a couple of unit tests

---
+++ test/ut/LocalBoardStorageTest.java
@@ -0,0 +1,34 @@
+package ut;
+
+import dal.localImplementation.LocalBoardStorage;
+import dalFactories.DALFactory;
+import domain.Enums.GameMode;
+import domain.Enums.PawnState;
+import org.junit.Assert;
+import org.junit.*;
+import org.junit.Test;
+
+import static org.junit.Assert.assertThat;
+
+public class LocalBoardStorageTest{
+    private LocalBoardStorage local;
+    @Before
+    public void Initialize(){
+        DALFactory dal = new DALFactory();
+
+        local = (LocalBoardStorage) dal.getLocalBoardStorage();
+    }
+
+    @Test
+    public void initTest(){
+        local.init(GameMode.FOURPLAYERBOARD);
+        Assert.assertNotNull(local.getTiles());
+    }
+
+    @Test
+    public void movePawn(){
+        local.getPawn(0).movePawnIntoPlay();
+
+        Assert.assertTrue(local.getPawn(0).getPawnState() == PawnState.INPLAY);
+    }
+}
