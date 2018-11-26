07672f10cc34937dc058aec879ae2d08ccd1f643
Besm <besmbesmbesm@gmail.com>
2018-11-26 14:50:09 +0100

    Update Message.java

--- src/main/java/shared/Message.java
+++ src/main/java/shared/Message.java
@@ -1,8 +1,11 @@
 package shared;
 
+import com.google.gson.Gson;
+
 public class Message {
     private String name;
     private Object[] data;
+    private static Gson gson = new Gson();
 
     public Message(String name, Object... data) {
         this.name = name;
@@ -15,5 +18,15 @@ public class Message {
 
     public Object[] getData() {
         return data;
+    }
+
+    public static Message fromJSON(String json){
+
+        return gson.fromJson(json,Message.class);
+    }
+
+    public String toJson(){
+
+        return gson.toJson(this);
     }
 }
