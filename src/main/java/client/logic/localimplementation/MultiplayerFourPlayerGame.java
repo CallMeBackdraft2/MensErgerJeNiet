b2a59da4b3f5ecbe4f7a5edcf4a8b3a215de8960
Besm <besmbesmbesm@gmail.com>
2018-12-02 20:03:42 +0100

    Finished client websocket communication

--- src/main/java/client/logic/localimplementation/MultiplayerFourPlayerGame.java
+++ src/main/java/client/logic/localimplementation/MultiplayerFourPlayerGame.java
@@ -1,29 +1,20 @@
 package client.logic.localimplementation;
 
-import client.dal.interfaces.BoardStorage;
-import client.dalfactories.DALFactory;
 import client.domain.classes.*;
-import client.domain.enums.GameMode;
 import client.logic.interfaces.Game;
 import client.websockets.CommunicatorWebSocket;
+import com.google.gson.Gson;
+import shared.Message;
 
-import java.util.List;
+import java.lang.reflect.Method;
 
 public class MultiplayerFourPlayerGame implements Game {
 
-    public Dice dice;
-    Lobby lobby;
-    BoardStorage boardStorage;
-    int currentTurn = 0;
-    private boolean diceRolled;
-    private boolean debugMode;
-    private boolean isDone;
-
     private CommunicatorWebSocket communicator;
+    private boolean needsUpdate;
 
-    public MultiplayerFourPlayerGame(){
-        boardStorage = DALFactory.getLocalBoardStorage();
-        boardStorage.init(GameMode.FOURPLAYERBOARD);
+    public MultiplayerFourPlayerGame() {
+
         communicator = new CommunicatorWebSocket(this);
 
         Thread communicatorThread = new Thread(communicator);
@@ -31,64 +22,124 @@ public class MultiplayerFourPlayerGame implements Game {
     }
 
 
+    public Message waitForResponse(String responseName) {
+        while (true) {
+            for (Message response : communicator.getResponses()) {
+                if (response.getName().equals(responseName)) {
+
+                    communicator.getResponses().remove(response);
+                    return response;
+                } else if(response.getName().equals("Exception")){
+                    System.out.println(response.getData()[0]);
+                }
+            }
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private <T> T call(Class<T> tClass, Object... args) {
+
+        StackTraceElement[] elements = Thread.currentThread().getStackTrace();
+        String methodName = elements[2].getMethodName();
+        Method method = null;
+        for (Method m : this.getClass().getMethods()) {
+            if (m.getName().equals(methodName)) {
+                method = m;
+            }
+        }
+
+        CommunicatorWebSocket.getInstance().sendMessage(new Message(methodName, args));
+        if(tClass == void.class){
+            return null;
+        }
+        Object responseData = waitForResponse(methodName).getData()[0];
+        assert method != null;
+        Gson gson = new Gson();
+
+
+        return gson.fromJson(gson.toJson(responseData), tClass);
+
+    }
+
+
+
     @Override
     public void skipTurn() {
+        call(void.class);
+    }
 
+    @Override
+    public boolean getNeedsUpdate() {
+        return needsUpdate;
     }
 
     @Override
     public int rollDice() {
-        return 0;
+        return call(int.class);
+    }
+
+    @Override
+    public int getDiceRolled() {
+       return call(int.class);
+    }
+
+    @Override
+    public int getDiceAmountRolled() {
+        return call(int.class);
     }
 
     @Override
     public void movePawn(String pawnId) {
-
+        call(void.class, pawnId);
     }
 
     @Override
-    public List<Tile> getTiles() {
-        return null;
+    public Tile[] getTiles() {
+        return call(Tile[].class);
     }
 
     @Override
-    public Tile getPossibleMove(Pawn pawn) {
-        return null;
+    public Tile getPossibleMove(String pawnId) {
+       return call(Tile.class,pawnId);
     }
 
     @Override
     public boolean isYourTurn() {
-        return false;
+       return true;
     }
 
     @Override
     public Pawn getPawn(String homeTileID) {
-        return null;
+      return   call(Pawn.class,homeTileID);
     }
 
     @Override
-    public List<Pawn> getPawns() {
-        return null;
+    public Pawn[] getPawns() {
+        return call(Pawn[].class);
     }
 
     @Override
     public int getCurrentPlayerId() {
-        return 0;
+       return call(int.class);
     }
 
     @Override
-    public List<Player> getPlayers() {
-        return null;
+    public Player[] getPlayers() {
+       return call(Player[].class);
     }
 
     @Override
     public void sendMessage(String message) {
-
+            call(void.class,message);
     }
 
     @Override
-    public List<String> getMessages() {
-        return null;
+    public String[] getMessages() {
+        return call(String[].class);
     }
 
     @Override
@@ -96,7 +147,7 @@ public class MultiplayerFourPlayerGame implements Game {
         return false;
     }
 
-    public void movePawn(String pawnId, String tileId){
-        boardStorage.movePawn(pawnId, tileId);
+    public void setNeedsUpdate(boolean needsUpdate) {
+        this.needsUpdate = needsUpdate;
     }
 }
--- src/main/java/client/websockets/CommunicatorWebSocket.java
+++ src/main/java/client/websockets/CommunicatorWebSocket.java
@@ -2,29 +2,29 @@ package client.websockets;
 
 import client.logic.localimplementation.MultiplayerFourPlayerGame;
 import com.google.gson.Gson;
-import com.google.gson.JsonSyntaxException;
 import shared.Message;
 
 import javax.websocket.*;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
 
 @ClientEndpoint
-public class CommunicatorWebSocket extends Communicator implements Runnable{
+public class CommunicatorWebSocket extends Communicator implements Runnable {
 
     // singleton
     private static CommunicatorWebSocket instance = null;
-
+    private static Session session;
+    private static String message;
+    private static Gson gson = null;
+    private static MultiplayerFourPlayerGame game;
+    private static List<Message> responses = new ArrayList<>();
     /*
      * The local websocket uri to connect to.
      */
     private final String uri = "ws://localhost:1234/communicator/";
-    private Session session;
-    private String message;
-    private Gson gson = null;
-    private MultiplayerFourPlayerGame game;
-
     // status of the websocket client
     boolean isRunning = false;
 
@@ -43,47 +43,59 @@ public class CommunicatorWebSocket extends Communicator implements Runnable{
      * Ensure that only one instance of this class is created.
      * @return instance of client web socket
      */
-    public static CommunicatorWebSocket getInstance(){
-        if(instance == null){
+    public static CommunicatorWebSocket getInstance() {
+        if (instance == null) {
             System.out.println("[WebSocket Client create singleton instance]");
             instance = new CommunicatorWebSocket();
         }
         return instance;
     }
 
-    /*
-     * start the connection
-     */
+    public static void main(String[] args) {
+        CommunicatorWebSocket communicator = new CommunicatorWebSocket();
+        communicator.start();
+    }
+
+    public void sendMessage(Message message) {
+
+        session.getAsyncRemote().sendText(message.toJson());
+    }
+
+    public List<Message> getResponses() {
+        return responses;
+    }
+
     public void start() {
         System.out.println("[WebSocket Client start connection]");
-        if(!isRunning){
+        if (!isRunning) {
             isRunning = true;
             startClient();
         }
     }
 
-    /*
-     * stop the connection
-     */
     public void stop() {
         System.out.println("[WebSocket Client stop]");
-        if(isRunning){
+        if (isRunning) {
             isRunning = false;
             stopClient();
         }
     }
 
     @OnOpen
-    public void onWebSocketConnect(Session session){
+    public void onWebSocketConnect(Session session) {
         System.out.println("[WebSocket Client open session] " + session.getRequestURI());
         this.session = session;
+        session.getAsyncRemote().sendText(new Message("getTiles").toJson());
     }
 
     @OnMessage
-    public void onWebSocketText(String message, Session session){
-        this.message = message;
-        System.out.println("[WebSocket Client message received] " + message);
-        processMessage(message);
+    public void onWebSocketText(String messageText, Session session) {
+        this.message = messageText;
+        Message message = Message.fromJSON(messageText);
+        if (message.getName().equals("update")) {
+            game.setNeedsUpdate(true);
+
+        } else responses.add(message);
     }
 
     @OnError
@@ -92,89 +104,29 @@ public class CommunicatorWebSocket extends Communicator implements Runnable{
     }
 
     @OnClose
-    public void onWebSocketClose(CloseReason reason){
+    public void onWebSocketClose(CloseReason reason) {
         System.out.print("[WebSocket Client close session] " + session.getRequestURI());
         System.out.println(" for reason " + reason);
         session = null;
     }
 
-
-    private void sendMessageToServer(){
-        String jsonMessage = gson.toJson("Henlo server");
-        // use async computing
-        session.getAsyncRemote().sendText(jsonMessage);
-    }
-
-    private void sendMessageToServer(String message){
-        String jsonMessage = gson.toJson(message);
-        // use async computing
-        session.getAsyncRemote().sendText(jsonMessage);
-    }
-
-
-    /*
-     * start a web socket client
-     */
-    private void startClient(){
+    private void startClient() {
         System.out.println("[WebSocket Client start]");
-        try{
+        try {
             WebSocketContainer container = ContainerProvider.getWebSocketContainer();
             container.connectToServer(this, new URI(uri));
-        } catch (DeploymentException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (URISyntaxException e) {
+        } catch (DeploymentException | URISyntaxException | IOException e) {
             e.printStackTrace();
         }
     }
 
-    /*
-     * stop the running client
-    */
-    private void stopClient(){
+    private void stopClient() {
         System.out.println("[WebSocket Client stop]");
-        try{
+        try {
             session.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-
-    // process incoming json message
-    private void processMessage(String jsonMessage){
-        // parse incoming message
-        //WebsocketMessage message;
-        Message message;
-        try{
-            message = gson.fromJson(jsonMessage, Message.class);
-        }catch (JsonSyntaxException e){
-            System.out.println("[WebSocket Client ERROR: cannot parse Json message " + jsonMessage);
-            return;
-        }
-
-        switch(message.getName()){
-            case "updatePawn" :
-                String pawnId = (String)message.getData()[0];
-                String tileId = (String)message.getData()[1];
-
-                game.movePawn(pawnId, tileId);
-                break;
-            case "yourId" :
-                int playerId = (int)message.getData()[0];
-                break;
-        }
-
-        // notify observers
-        this.setChanged();
-        this.notifyObservers(message);
-    }
-
-    public static void main(String[] args){
-        CommunicatorWebSocket communicator = new CommunicatorWebSocket();
-        communicator.start();
-        communicator.sendMessageToServer();
     }
 
     @Override
@@ -183,7 +135,4 @@ public class CommunicatorWebSocket extends Communicator implements Runnable{
         communicator.start();
     }
 
-    public void movePawn(String pawnId, String tileId){
-        sendMessageToServer(gson.toJson(new Message("movePawn", new Object[]{ pawnId, tileId })));
-    }
 }
