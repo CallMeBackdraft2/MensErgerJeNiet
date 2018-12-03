5a2c9092955a0a941ef27847ed62957b71c2ffec
Houssem <Houssem@dodor.nl>
2018-12-03 14:15:59 +0100

    LoginView met controller toegevoegd

---
+++ src/ui/LoginViewController.java
@@ -0,0 +1,21 @@
+package ui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class LoginViewController {
+    @FXML Button bttn_register;
+    @FXML Button bttn_login;
+    @FXML TextField txt_password;
+    @FXML TextField txt_username;
+
+    public void bttn_register_click(ActionEvent event) {
+
+    }
+
+    public void bttn_login_click(ActionEvent event){
+
+    }
+}
