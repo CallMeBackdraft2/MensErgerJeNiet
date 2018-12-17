f5db3500bfb70f9808bca2ecaf51765391bb5b9d
Besm <besmbesmbesm@gmail.com>
2018-12-17 14:44:33 +0100

    REST login and register methods using UUID

--- src/main/java/server/rest/Service.java
+++ src/main/java/server/rest/Service.java
@@ -1,5 +1,6 @@
 package server.rest;
 
+import client.domain.classes.Player;
 import com.google.gson.Gson;
 import shared.rest.CSAbsResponse;
 import shared.rest.SLoginResponse;
@@ -9,65 +10,45 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
-@Path("/login")
+import static java.util.UUID.nameUUIDFromBytes;
+
+@Path("")
 public class Service {
-    ArrayList<SLoginResponse> users = MejnServer.users;
+
+    private static Map<String, Player> playerHashMap = new HashMap<>();
+
 
     @GET
-    @Path("/{value}")
-    public Response getMsg(@PathParam("value") String msg) {
-        /**
-        CSAbsResponse response = new CSAbsResponse();
-        response.setOperation("abs");
-        response.setExpression(msg);
-        try {
-            int value = Integer.parseInt(msg);
-            int result = (value<0)? -value:value;
-            response.setResult(Integer.toString(result));
-        } catch (NumberFormatException nfe) {
-            response.setResult("invalid value");
-        }
-         **/
+    @Path("users/login/{username}/{password}")
+    public Response login(@PathParam("username") String username,
+                          @PathParam("password") String password) {
 
-        SLoginResponse loginResponse = null;
-        if(msg.contains("register")) {
-            String[] parts = msg.split("-");
-            int userindex = -1;
-            boolean exist = false;
-            for(int i = 0; i < users.size(); i++){
-                if(users.get(i).getUsername().equals(parts[1])){
-                    userindex = i;
-                    System.out.println("true");
-                    exist = true;
-                } else{
-                    System.out.println(parts[1] + " is not the same as " + users.get(i).getUsername());
-                }
-            }
-            if(exist){
-                if(parts[2] == users.get(userindex).getPassword()) {
-                    loginResponse = users.get(userindex);
-                    loginResponse.setResult("logged in");
-                } else{
-                    loginResponse = users.get(userindex);
-                    loginResponse.setResult("incorrect password");
-                }
+        Player p = playerHashMap.get(generateUUID(username,password));
 
-            } else {
-                System.out.println("false");
-                loginResponse = new SLoginResponse();
-                loginResponse.setUsername(parts[1]);
-                loginResponse.setPassword(parts[2]);
-                loginResponse.setResult("registered");
-                users.add(loginResponse);
-            }
-        } else {
-            loginResponse = new SLoginResponse();
-            loginResponse.setUsername(msg);
-            loginResponse.setResult("success");
-        }
-        Gson gson = new Gson();
-        String output = gson.toJson(loginResponse);
-        return Response.status(200).entity(output).build();
+        return Response.status(200).entity(p.getName()).build();
+    }
+
+
+    private String generateUUID(String username, String password){
+        byte[] bytes =  (username+"||"+password).getBytes();
+       UUID uuid = UUID.nameUUIDFromBytes(bytes);
+       return uuid.toString();
+    }
+
+    @GET
+    @Path("users/register/{username}/{password}")
+    public Response register(@PathParam("username") String username,
+                             @PathParam("password") String password) {
+
+
+        String id =  generateUUID(username,password);
+        playerHashMap.put(id,new Player(username));
+
+
+        return Response.status(200).entity(id).build();
     }
 }
